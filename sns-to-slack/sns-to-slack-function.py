30dbcbe3d692bcc6ea36c51d518f4390576ce183
rohit.patidar <rohit.patidar@sainsburys.co.uk>
2022-07-05 11:27:07 +0100

    updated lambda function

--- sns-to-slack/sns-to-slack-function.py
+++ sns-to-slack/sns-to-slack-function.py
@@ -1,32 +1,197 @@
-import json
-import re
 import os
-import boto3
-import urllib3
+import json
+import base64
+import re
+import requests
 
-# Lambda global variables
-region = os.environ["AWS_REGION"]  # from Lambda default envs
-slack_url = os.environ["SLACK_URL"]
-slack_channel = os.environ["SLACK_CHANNEL"]
-slack_user = os.environ["SLACK_USER"]
+from base64 import b64decode
+
+DEFAULT_USERNAME = os.environ.get('DEFAULT_USERNAME', 'AWS Lambda')
+DEFAULT_CHANNEL = os.environ.get('DEFAULT_CHANNEL', '#slack-mdm-dev')
+DEFAULT_EMOJI = os.environ.get('DEFAULT_EMOJI', ':information_source:')
+USERNAME_PREFIX = os.environ.get('USERNAME_PREFIX', '')
 
 
-http = urllib3.PoolManager()
+def get_slack_channel(region, event_src, topic_name, channel_map):
+    '''Map region and event type to Slack channel name
+    '''
+    try:
+        return channel_map[topic_name]
+    except KeyError:
+        return DEFAULT_CHANNEL
+
+
+def get_slack_username(event_src):
+    '''Map event source to the Slack username
+    '''
+    username_map = {
+        'cloudwatch': 'AWS CloudWatch',
+        'autoscaling': 'AWS AutoScaling',
+        'elasticache': 'AWS ElastiCache',
+        'codepipeline': 'AWS CodePipeline',
+        's3': 'AWS S3'}
+
+    try:
+        return "{0}{1}".format(USERNAME_PREFIX, username_map[event_src])
+    except KeyError:
+        return DEFAULT_USERNAME
 
 
 def lambda_handler(event, context):
-    url = slack_url
-    msg = {
-        "channel": slack_channel,
-        "username": slack_user,
-        "text": event['Records'][0]['Sns']['Message'],
-        "icon_emoji": ""
+    '''The Lambda function handler
+    '''
+    config = {
+      'webhook_url': os.environ['WEBHOOK_URL'],
+      'channel_map': json.loads(base64.b64decode(os.environ['CHANNEL_MAP']))
     }
 
-    encoded_msg = json.dumps(msg).encode('utf-8')
-    resp = http.request('POST', url, body=encoded_msg)
-    print({
-        "message": event['Records'][0]['Sns']['Message'],
-        "status_code": resp.status,
-        "response": resp.data
-    })
+    event_cond = 'default'
+    sns = event['Records'][0]['Sns']
+    print('DEBUG EVENT:', sns['Message'])
+    try:
+        json_msg = json.loads(sns['Message'])
+    except ValueError as e:
+        json_msg = {}
+
+    if sns['Subject']:
+        message = sns['Subject']
+    else:
+        message = sns['Message']
+
+    attachments = []
+    if json_msg.get('AlarmName'):
+        event_src = 'cloudwatch'
+        event_cond = json_msg['NewStateValue']
+        color_map = {
+            'OK': 'good',
+            'INSUFFICIENT_DATA': 'warning',
+            'ALARM': 'danger'
+        }
+
+        attachments = [{
+            'fallback': json_msg,
+            'message': json_msg,
+            'color': color_map[event_cond],
+            "fields": [{
+                "title": "Alarm",
+                "value": json_msg['AlarmName'],
+                "short": True
+            }, {
+                "title": "Status",
+                "value": json_msg['NewStateValue'],
+                "short": True
+            }, {
+                "title": "Description",
+                "value": json_msg['AlarmDescription'],
+                "short": False
+            }, {
+                "title": "Reason",
+                "value": json_msg['NewStateReason'],
+                "short": False
+            }]
+        }]
+    elif json_msg.get('Cause'):
+        event_src = 'autoscaling'
+        attachments = [{
+            "text": "Details",
+            "fallback": message,
+            "color": "good",
+            "fields": [{
+                "title": "Capacity Change",
+                "short": True
+            }, {
+                "title": "Event",
+                "value": json_msg['Event'],
+                "short": False
+            }, {
+                "title": "Cause",
+                "value": json_msg['Cause'],
+                "short": False
+            }]
+        }]
+    elif json_msg.get('ElastiCache:SnapshotComplete'):
+        event_src = 'elasticache'
+        attachments = [{
+            "text": "Details",
+            "fallback": message,
+            "color": "good",
+            "fields": [{
+                "title": "Event",
+                "value": "ElastiCache Snapshot"
+            }, {
+                "title": "Message",
+                "value": "Snapshot Complete"
+            }]
+        }]
+    elif re.match("S3", sns.get('Subject') or ''):
+        event_src = 's3'
+        attachments = [{
+            "fields": [{
+                "title": "Source",
+                "value": "{0} '{1}'".format(json_msg['Event Source'], json_msg['Source ID'])
+                },{
+                "title": "Message",
+                "value": json_msg['Event Message']
+                }]}]
+        if json_msg.get('Identifier Link'):
+            title_arr = json_msg['Identifier Link'].split('\n')
+            if len(title_arr) >= 2:
+                title_str = title_arr[1]
+                title_lnk_str = title_arr[0]
+            else:
+                title_str = title_lnk_str = title_arr[0]
+            attachments[0]['fields'].append({
+                "title": "Details",
+                "value": "<{0}|{1}>".format(title_str, title_lnk_str)
+            })
+    elif json_msg.get('source') == 'aws.codepipeline':
+        event_src = 'codepipeline'
+        message = json_msg.get('detail-type')
+        event_cond = json_msg.get('detail').get('state')
+        color_map = {
+            'STARTED': 'good',
+            'SUCCEEDED': 'good',
+            'FAILED': 'danger'
+        }
+        attachments = [{
+            'fallback': json_msg.get('detail-type'),
+            'color': color_map[event_cond],
+            "fields": [{
+                "title": "Pipeline",
+                "value": json_msg.get('detail').get('pipeline')
+            }, {
+                "title": "State",
+                "value": json_msg.get('detail').get('state')
+            }]
+        }]
+    else:
+        event_src = 'other'
+
+    # SNS Topic ARN: arn:aws:sns:<REGION>:<AWS_ACCOUNT_ID>:<TOPIC_NAME>
+    #
+    # SNS Topic Names => Slack Channels
+    #  <env>-alerts => alerts-<region>
+    #  <env>-notices => events-<region>
+    #
+    region = sns['TopicArn'].split(':')[3]
+    topic_name = sns['TopicArn'].split(':')[-1]
+    # event_env = topic_name.split('-')[0]
+    # event_sev = topic_name.split('-')[1]
+
+    # print('DEBUG:', topic_name, region, event_env, event_sev, event_src)
+
+    channel_map = config['channel_map']
+
+    payload = {
+        'text': message,
+        'channel': get_slack_channel(region, event_src, topic_name, channel_map),
+        'username': get_slack_username(event_src)}
+    if attachments:
+        payload['attachments'] = attachments
+    print('DEBUG PAYLOAD:', json.dumps(payload))
+
+    webhook_url = config['webhook_url'] if re.match('^https://', config['webhook_url']) \
+        else f"https://{config['webhook_url']}"
+    r = requests.post(webhook_url, json=payload)
+
+    return r.status_code
